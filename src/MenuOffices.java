f8bdf3b6ce86bb40b7bc1930e5ebca65e1413fd1
jan1310 <jan.schiffer@hsw-stud.de>
2021-11-03 11:29:43 +0100

    added menuOffices

--- src/MenuOffices.java
+++ src/MenuOffices.java
@@ -2,12 +2,12 @@ import gmbh.kdb.hsw.gdp.domain.GameDevStudio;
 import gmbh.kdb.hsw.gdp.domain.Office;
 
 public class MenuOffices {
-    public static String showOffices(GameDevStudio studio){
+    public static void showOffices(GameDevStudio studio){
         String offices = "";
         for (Office office : studio.getOffices()) {
             offices.concat(printOffice(office));
         }
-        return offices;
+        System.out.println(offices);
     }
     private static String printOffice(Office office){
         return
