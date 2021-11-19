e846de1153e39851a4f047ccfaf5009da807bded
jan1310 <jan.schiffer@hsw-stud.de>
2021-11-19 12:02:33 +0100

    Jan: used static method employees

--- src/MenuDeveloper.java
+++ src/MenuDeveloper.java
@@ -6,18 +6,9 @@ import java.util.List;
 
 public class MenuDeveloper {
     public static void showDeveloper(GameDevStudio studio) {
-        var allDeveloper = new ArrayList<Developer>();
-
-        var offices = studio.getOffices();
-        for (int i = 0; i < offices.size(); i++) {
-            var developer = offices.get(i).getDevelopers();
-            for (int k = 0; k < developer.size(); k++) {
-                allDeveloper.add(developer.get(k));
-            }
-        }
-        System.out.println("Number of Developer: " + allDeveloper.size());
-        for (int i = 0; i < allDeveloper.size(); i++) {
-            System.out.println((i + 1) + ". " + allDeveloper.get(i));
+        System.out.println("Number of Developer: " + Employees.getEmployees(studio).size());
+        for (int i = 0; i < Employees.getEmployees(studio).size(); i++) {
+            System.out.println((i + 1) + ". " + Employees.getEmployees(studio).get(i));
         }
     }
 }
