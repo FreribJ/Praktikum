030e8d3937ba3d700af9fbfbc23c537b1a6457b3
jan1310 <jan.schiffer@hsw-stud.de>
2021-12-01 10:16:29 +0100

    Jan

--- src/SpecialApplication.java
+++ src/SpecialApplication.java
@@ -1,9 +1,17 @@
-import gmbh.kdb.hsw.gdp.domain.Application;
-import gmbh.kdb.hsw.gdp.domain.Developer;
-import gmbh.kdb.hsw.gdp.domain.Money;
+import gmbh.kdb.hsw.gdp.domain.*;
+
+import java.math.BigDecimal;
 
 public class SpecialApplication extends Application {
+
     public SpecialApplication(Developer developer, Money hireBonus, Money hireAgentFee) {
         super(developer, hireBonus, hireAgentFee);
+    }
+
+    public static SpecialApplication createSpecialApplication(Skillset skills, double hireBonus, double hireAgentFee, String name, double salary){
+        var developer = new Developer(new DeveloperName(name), new Money(new BigDecimal(0)), Happiness.create(), null, new Day(0), skills);
+        var hireBonusMoney = new Money(new BigDecimal(hireBonus));
+        var hireAgentFeeMoney = new Money(new BigDecimal(hireAgentFee));
+        return new SpecialApplication(developer, hireBonusMoney, hireAgentFeeMoney);
     }
 }
