710b23c0e21dfd8f5ad645a8fab5e9662d8213b3
jan1310 <jan.schiffer@hsw-stud.de>
2021-12-11 16:39:34 +0100

    Changes by freri

--- src/praktikum/TextHandler.java
+++ src/praktikum/TextHandler.java
@@ -1,5 +1,7 @@
 package praktikum;
 
+import gmbh.kdb.hsw.gdp.domain.Skillset;
+
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
@@ -12,6 +14,7 @@ public class TextHandler {
 
     /**
      * Reads a user input from the console as a {@link String}.
+     *
      * @param message for a user input.
      * @return the user input as a {@link String}.
      */
@@ -23,7 +26,7 @@ public class TextHandler {
         System.out.println("-".repeat(100));
 
         //Für Konsoleneingabe über Batch
-        if (input.length() > 0 && input.toCharArray()[input.toCharArray().length-1] == 13) {
+        if (input.length() > 0 && input.toCharArray()[input.toCharArray().length - 1] == 13) {
             input = input.substring(0, input.length() - 1);
         }
         return input;
@@ -31,42 +34,43 @@ public class TextHandler {
 
     /**
      * Reads a user input from the console until it is a {@link Integer}
+     *
      * @param message for a user input
      * @return the user input as a {@link Integer}
      */
-    public static int getInt(String message){
-        System.out.println();
-        System.out.println(message);
-            try {
-                String input = sc.next();
-
-                //Für Konsoleneingabe über Batch
-                if (input.length() > 0 && input.toCharArray()[input.toCharArray().length-1] == 13) {
-                    input = input.substring(0, input.length() - 1);
-                }
-
-                int intInput = Integer.parseInt(input);
-                System.out.println();
-                System.out.println("-".repeat(100));
-                return intInput;
-            }catch (NumberFormatException e){
-                return getInt("you have to give a number:");
-            }
-    }
-
-    /**
-     * Reads a user input from the console until it is a {@link Double}
-     * @param message for a user input
-     * @return the user input as a {@link Double}
-     */
-    public static double getDouble(String message){
+    public static int getInt(String message) {
         System.out.println();
         System.out.println(message);
         try {
             String input = sc.next();
 
             //Für Konsoleneingabe über Batch
-            if (input.length() > 0 && input.toCharArray()[input.toCharArray().length-1] == 13) {
+            if (input.length() > 0 && input.toCharArray()[input.toCharArray().length - 1] == 13) {
+                input = input.substring(0, input.length() - 1);
+            }
+
+            System.out.println();
+            System.out.println("-".repeat(100));
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            return getInt("you have to give a number:");
+        }
+    }
+
+    /**
+     * Reads a user input from the console until it is a {@link Double}
+     *
+     * @param message for a user input
+     * @return the user input as a {@link Double}
+     */
+    public static double getDouble(String message) {
+        System.out.println();
+        System.out.println(message);
+        try {
+            String input = sc.next();
+
+            //Für Konsoleneingabe über Batch
+            if (input.length() > 0 && input.toCharArray()[input.toCharArray().length - 1] == 13) {
                 input = input.substring(0, input.length() - 1);
             }
 
@@ -80,10 +84,29 @@ public class TextHandler {
     }
 
     /**
+     * Reads 4 skills to create a {@link Skillset}
+     * @return the combined {@link Skillset}
+     */
+    public static Skillset getSkillset() {
+        int[] skills = new int[4];
+        String[] skillNames = {"Coding", "Research", "Testing", "Design"};
+
+        for (int i = 0; i < 4; i++) {
+            skills[i] = TextHandler.getInt(skillNames[i] + " skill [0 - 10]: ");
+            while (skills[i] > 10 || skills[i] < 0) {
+                skills[i] = TextHandler.getInt("A skill must be between 0 - 10: ");
+            }
+        }
+
+        return new Skillset(skills[0],skills[1],skills[2],skills[3]);
+    }
+
+    /**
      * Prints an output at the console in a unified design
+     *
      * @param text is the information, stored as a {@link String}.
      */
-    public static void print(String text){
+    public static void print(String text) {
         var textList = new ArrayList<String>();
         textList.add(text);
         print(textList);
@@ -91,80 +114,86 @@ public class TextHandler {
 
     /**
      * Prints an output at the console in a unified design
+     *
      * @param textList has the main information, stored as a {@link ArrayList} of {@link String}s.
      */
-    public static void print(ArrayList<String> textList){
+    public static void print(ArrayList<String> textList) {
         print(textList, null);
     }
 
     /**
      * Prints an output at the console in a unified design
+     *
      * @param textList has the main information, stored as a {@link ArrayList} of {@link String}s.
      * @param headline is for the headline above the information.
      */
-    public static void print(ArrayList<String> textList, String headline){
+    public static void print(ArrayList<String> textList, String headline) {
         print(textList, headline, false);
     }
 
     /**
      * Prints an output at the console in a unified design
-     * @param textList has the main information, stored as a {@link ArrayList} of {@link String}s.
-     * @param headline is for the headline above the information.
+     *
+     * @param textList    has the main information, stored as a {@link ArrayList} of {@link String}s.
+     * @param headline    is for the headline above the information.
      * @param enumeration if enumeration is true all main information will be counted.
      */
-    public static void print(ArrayList<String> textList, String headline,  boolean enumeration){
+    public static void print(ArrayList<String> textList, String headline, boolean enumeration) {
         print(textList, headline, null, enumeration, null);
     }
 
     /**
      * Prints an output at the console in a unified design
+     *
      * @param textList has the main information, stored as a {@link ArrayList} of {@link String}s.
      * @param headline is for the headline above the information.
-     * @param prefix is for a unified text in front of the main information.
+     * @param prefix   is for a unified text in front of the main information.
      */
-    public static void print(ArrayList<String> textList, String headline,  String prefix){
+    public static void print(ArrayList<String> textList, String headline, String prefix) {
         print(textList, headline, prefix, false, null);
     }
 
     /**
      * Prints an output at the console in a unified design
-     * @param textList has the main information, stored as a {@link ArrayList} of {@link String}s.
+     *
+     * @param textList    has the main information, stored as a {@link ArrayList} of {@link String}s.
      * @param enumeration if enumeration is true all main information will be counted.
      */
-    public static void print(ArrayList<String> textList, boolean enumeration){
+    public static void print(ArrayList<String> textList, boolean enumeration) {
         print(textList, null, enumeration);
     }
 
     /**
      * Prints an output at the console in a unified design
-     * @param textList has the main information, stored as a {@link ArrayList} of {@link String}s.
-     * @param headline is for the headline above the information.
-     * @param prefix is for a unified text in front of the main information.
-     * @param enumeration if enumeration is true all main information will be counted.
+     *
+     * @param textList         has the main information, stored as a {@link ArrayList} of {@link String}s.
+     * @param headline         is for the headline above the information.
+     * @param prefix           is for a unified text in front of the main information.
+     * @param enumeration      if enumeration is true all main information will be counted.
      * @param extraInformation is for extra information for every main information.
      *                         Extra information for one main information are stored as a {@link ArrayList} of {@link String}s.
      *                         Each extra information for one main information are also stored in a {@link ArrayList}.
      */
-    public static void print(ArrayList<String> textList, String headline, String prefix, boolean enumeration, ArrayList<ArrayList<String>> extraInformation) throws RuntimeException{
-        if(prefix == null)
-                prefix = "";
-        if(Objects.equals(headline, ""))
-                headline = null;
+    public static void print(ArrayList<String> textList, String headline, String prefix, boolean enumeration, ArrayList<ArrayList<String>> extraInformation) throws RuntimeException {
+        if (prefix == null)
+            prefix = "";
+        if (Objects.equals(headline, ""))
+            headline = null;
         System.out.println();
-        if(headline != null){
+        if (headline != null) {
             System.out.println(headline + ":");
         }
-        if(!prefix.equals("")){
+        if (!prefix.equals("")) {
             prefix = prefix + ": ";
         }
-        for(int i = 0; i < textList.size(); i++) {
-            System.out.println((enumeration ? i+1 + ". " : "") + prefix + textList.get(i));
-            if(extraInformation != null){
-                if(textList.size() != extraInformation.size()){
+        for (int i = 0; i < textList.size(); i++) {
+            System.out.println((enumeration ? i + 1 + ". " : "") + prefix + textList.get(i));
+            if (extraInformation != null) {
+                if (textList.size() != extraInformation.size()) {
                     throw new RuntimeException("textList and extraInformation must have the same length");
                 }
                 var extraInformation2D = extraInformation.get(i);
-                for(String information : extraInformation2D){
+                for (String information : extraInformation2D) {
                     System.out.println("   " + information);
                 }
             }
